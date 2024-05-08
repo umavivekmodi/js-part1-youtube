cfe68e59fb0312d217f480e24e1724f34416a7b2
UmaModi <uumad.modi1996@gmail.com>
2024-05-08 08:22:04 +0000

    dataTypes

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all js code as  newer version
+
+// alert(3+3) // we are using nodejs , not browser 
+
+console.log(3+3) //code readability should be high
+
+console.log("UmaModi")
+
+let age = 25
+
+let isLoggedIn = false
+
+let state = null
+
+// premitive data type:-
+// number => 2 to power 53
+// bigint is use for too much big number its use  in trade market,trading , facebook, in big website, stock market
+// string =>""
+// boolean => true/false
+// null => standalone value - is a stand alone value also a data type, representation of empty value 
+// undefined => value is not assined
+// symbol => unique
+
+//object 
+
+console.log(typeof "UmaModi");
+
+console.log(typeof age);
+
+console.log(typeof undefined);  //undefined
+
+console.log(typeof null); //object
+
