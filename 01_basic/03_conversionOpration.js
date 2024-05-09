7956ec037eabf551cc2f3e01a164f502e6787bd7
UmaModi <uumad.modi1996@gmail.com>
2024-05-09 06:55:50 +0000

    conversionOpration

---
+++ 01_basic/03_conversionOpration.js
@@ -0,0 +1,22 @@
+//let score ="UmaModi"        //true       //undefined      //null  //"33abc"
+
+//console.log (typeof score);
+//console.log (typeof(score));
+
+// let  matchScore = "35"
+ 
+// console.log( typeof matchScore);  the result is string
+
+//let valueInNumber = Number(score)
+//console.log( typeof valueInNumber);
+//console.log(valueInNumber);   //not a number (NaN)
+  
+
+//"33" => 33
+//"33abc" => NaN
+//true = 1; false=> 0
+
+ let IsLoggedIn= 1    //"33"
+let booleanIsLoggedIn = Boolean(IsloggedIn)
+console.log(booleanIsLoggedIn);
+// 03_conversionOpration.js
