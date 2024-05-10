0f1c6f503f8203794a15a58a7c145b9ecdcca347
UmaModi <uumad.modi1996@gmail.com>
2024-05-10 09:26:23 +0000

    try some opration on oprand

--- 01_basic/03_conversionOpration.js
+++ 01_basic/03_conversionOpration.js
@@ -28,3 +28,52 @@ let stringNumber = string(Number)
 
 console.log(stringNumber);
 console.log( typeof stringNumber);
+
+
+
+// *******************OPRATIONS************************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let v1="hello"
+let v2 =" Uma"
+
+let v3= v1 + v2
+// console.log(v3)
+
+
+// console.log("1" + 2)
+// console.log( 1 + "2")
+// console.log("2" + "2")
+// console.log("1"+ 2 + 2)
+// console.log(2 + 2 + "1")
+
+// console.log((3 + 4 ) * 5 % 3)
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+// console.log(num3)
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+// link to study
+// https://tc39.es/ecma262/2023/#sec-type-conversion
