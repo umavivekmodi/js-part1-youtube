5b618b5f26a3c15eec071c5ffea170fbe88b3e09
UmaModi <uumad.modi1996@gmail.com>
2024-05-17 12:38:14 +0000

    modify

--- 01_basic/dataTypes-summary.js
+++ 01_basic/dataTypes-summary.js
@@ -1,61 +1,61 @@
-// kis trh se memory me data ko rakha jata hai or access kiya jata hai usi aadhar pr 2 prakar ke data types divide kiye gye hai
-// primitive or non - primitive 
-//  Primitive  call by value -copy me change kiya jata reference me nahi 
-// 7 types : string , Number , Boolean ,Null , undefined ,symbol, BigInt
+// // kis trh se memory me data ko rakha jata hai or access kiya jata hai usi aadhar pr 2 prakar ke data types divide kiye gye hai
+// // primitive or non - primitive 
+// //  Primitive  call by value -copy me change kiya jata reference me nahi 
+// // 7 types : string , Number , Boolean ,Null , undefined ,symbol, BigInt
 
-const score = 100
-const scoreValue = 100.34
+// const score = 100
+// const scoreValue = 100.34
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = symbol ('123')
-const anotherId =  symbol('123')
-//  console.log (id === anotherId);
+// const id = symbol ('123')
+// const anotherId =  symbol('123')
+// //  console.log (id === anotherId);
 
-//  const bigNumber = 32456787734578387484787348n
+// //  const bigNumber = 32456787734578387484787348n
 
-// reference type ( non- primitive)  direct memory me reference allocate kiya ja skta hai
-// Array, Objects , Funcations
+// // reference type ( non- primitive)  direct memory me reference allocate kiya ja skta hai
+// // Array, Objects , Funcations
 
-const heros = [ "shaktiman","naagraj", "doga"];
- let myObj = {
-    name: "uma,
-    age : 25 ,
-}
+// const heros = ["shaktiman","naagraj", "doga"];
+//  let myObj = {
+//     name : "uma"
+//     age : 25,
+// }
 
- const myFuncation = funcation ( ) {
-//  console.log( "hello world");
- }
-
-
- console.log( typeof outsideTemp);
+//  const myFuncation = funcation ( ) {
+// //   console.log("hello world");
+//  }
 
 
+//  console.log( typeof outsideTemp);
 
- //  +++++++++++++++++++++++++++ MEMORY+++++++++++++++++++++++++++++++++++++++++
 
-// Stack(primitive)  = variable declear kiya h uski copy milti h, 
-// Heap (Non- primitive)= original value ka  reference means wo original value me change hoga
 
-let myYouTubeName = "vivekmodidotcom"
+//  //  +++++++++++++++++++++++++++ MEMORY+++++++++++++++++++++++++++++++++++++++++
 
-let anotherName = myYouTubeName
-anothername = "uvcode"
+// // Stack(primitive)  = variable declear kiya h uski copy milti h, 
+// // Heap (Non- primitive)= original value ka  reference means wo original value me change hoga
 
-console.log(anotherName);
-console.log(myYouTubeName);
+// let myYouTubeName = "vivekmodidotcom"
 
-let userone = {
-    email: "user@google.com"
-    UPI : "user@ybl"
-}
+// let anotherName = "myYouTubeName"
+//    anotherName = "uvcode"
 
-let userTwo= userone
+// console.log(anotherName);
+// console.log(myYouTubeName);
+
+// let userOne = 
+// {
+//     email: "user@google.com"
+//     UPI : "user@ybl"
+// }
+
+// let userTwo= userOne
  
- userTwo.email = "umesh@gmail.com"
-
- console.log(userone.email);
- console.log(userTwo.email);
+//  userTwo.email = "uma@gmail.com"
+//  console.log(userOne.email);
+//  console.log(userTwo.email);
 
