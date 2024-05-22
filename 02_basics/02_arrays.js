f86bdcbed4854a4a6bebcfa64392b7d7d98db361
UmaModi <uumad.modi1996@gmail.com>
2024-05-22 16:31:10 +0000

    arrAY

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -18,14 +18,19 @@ const dc_Heros = ["superman", "flash","batman"]
 // const all_new_heros=[...marvel_Heros,...dc_Heros]  //spread oprater use to add arrays  to use = ..... Array name,.... array2name
 // console.log(all_new_heros);
 
-const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
-const real_another_array = another_array.flat(Infinity)
+// const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
 
-console.log(real_another_array);
+// console.log(real_another_array);
 
-console.log(Array.isArray("hitesh"));   //for ask it is a array or not 
-console.log(Array.from("hitesh"));   // to convert in an array  we can use objects and strings and more 
-console.log(Array.isArray({name = "hitesh"}));
+// console.log(Array.isArray("hitesh"));   //for ask it is a array or not 
+// console.log(Array.from("hitesh"));   // to convert in an array  we can use objects and strings and more 
+// console.log(Array.isArray({name = "hitesh"}));  //intrusting case
 
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
+ //  need more reading about  isArray(), from() , of()
 
 
