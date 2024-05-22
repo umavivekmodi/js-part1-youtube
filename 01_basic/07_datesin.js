64f3a479007400020850356b64bc228e2d3061a1
UmaModi <uumad.modi1996@gmail.com>
2024-05-22 11:57:06 +0000

    read the date funcation

---
+++ 01_basic/07_datesin.js
@@ -0,0 +1,43 @@
+// date is an object type
+
+let myDate = new Date()
+
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeOf myDate);          error
+// let myCreatedDate =  new Date(2024,0,24)
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate =  new Date(2024,0,24,5,3)
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate =  new Date("2024-01-14")  //yyyy-mm-dd
+// console.log(myCreatedDate.toLocaleString());
+// let myCreatedDate =  new Date("05-22-2014")  //mm-dd-yyyy
+// console.log(myCreatedDate.toLocaleString());
+// let myCreatedDate =  new Date("05-22-2014")  //mm-dd-yyyy
+// console.log(myCreatedDate.getTime());
+
+
+
+let myTimeStamp =Date.now();
+// console.log(myTimeStamp);
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+// console.log(`${newDate.getDate() $ {myTimeStamp.getTime()}`);  error
+
+
+
+
+
+
+
+
+
+
