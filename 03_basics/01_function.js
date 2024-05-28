afc98bf43a617a1994d2e02231a075257d341349
UmaModi <uumad.modi1996@gmail.com>
2024-05-28 08:12:05 +0000

    function part 1

---
+++ 03_basics/01_function.js
@@ -0,0 +1,26 @@
+
+// function SayMyName(){
+// console.log("H");
+// console.log("I");
+// console.log("T");
+// console.log("E");
+// console.log("S");
+// console.log("H");
+// }
+// SayMyName()
+
+// function addTwoNumbers(number1 , number2)   // when function is define its a =>perameters
+// {
+//        console.log(number1 + number2);
+// }
+function addTwoNumbers(number1 , number2)   // when function is define its a =>perameters
+{
+//        let result = number1 + number2
+//        return result
+        return number1 + number2
+}
+ const result = addTwoNumbers(3,5)           // when function call its an => arguments
+
+// console.log("Result:" , result);
+
+function loginusermassage (userName)
